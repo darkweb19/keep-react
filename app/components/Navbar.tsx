fe4890d607dec9794f43954bfad209308e633d70
WooFoo <sujanstha3220@gmail.com>
2024-02-27 18:28:05 +0545

    Feature : Inside the body , dark-theme class is added

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -10,8 +10,17 @@ import Search from './Search'
 
 const Navbar = () => {
   const [active, setActive] = useState(false)
+  const [theme, setTheme] = useState('dark-theme')
   const [showModal, setShowMainModal] = useState(false)
   const pathname = usePathname()
+
+  const handleTheme = () => {
+    if (theme === 'dark-theme') {
+      setTheme('light-theme')
+    } else {
+      setTheme('dark-theme')
+    }
+  }
 
   const IsActive = (str: string) => {
     const lastPart = pathname.toLocaleLowerCase().split('/').pop()
@@ -21,7 +30,8 @@ const Navbar = () => {
   useEffect(() => {
     setActive(false)
     setShowMainModal(false)
-  }, [pathname])
+    document.body.className = theme
+  }, [pathname, theme])
 
   const handleModal = () => {
     setShowMainModal(!showModal)
@@ -90,6 +100,14 @@ const Navbar = () => {
               className="group h-min w-fit justify-center rounded-md border border-metal-900  bg-metal-900 px-4 py-2.5 text-center text-body-5 font-medium capitalize text-white transition-all duration-75 ease-in hover:bg-metal-800 focus:ring-4 focus:ring-metal-800 active:bg-metal-900">
               get started
             </Link>
+            <a
+              target="_blank"
+              onClick={() => handleTheme()}
+              className="rounded grayscale transition duration-300 hover:bg-primary-50 hover:grayscale-0 sm:bg-primary-25 sm:p-3">
+              <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="#000000" viewBox="0 0 256 256">
+                <path d="M120,40V16a8,8,0,0,1,16,0V40a8,8,0,0,1-16,0Zm72,88a64,64,0,1,1-64-64A64.07,64.07,0,0,1,192,128Zm-16,0a48,48,0,1,0-48,48A48.05,48.05,0,0,0,176,128ZM58.34,69.66A8,8,0,0,0,69.66,58.34l-16-16A8,8,0,0,0,42.34,53.66Zm0,116.68-16,16a8,8,0,0,0,11.32,11.32l16-16a8,8,0,0,0-11.32-11.32ZM192,72a8,8,0,0,0,5.66-2.34l16-16a8,8,0,0,0-11.32-11.32l-16,16A8,8,0,0,0,192,72Zm5.66,114.34a8,8,0,0,0-11.32,11.32l16,16a8,8,0,0,0,11.32-11.32ZM48,128a8,8,0,0,0-8-8H16a8,8,0,0,0,0,16H40A8,8,0,0,0,48,128Zm80,80a8,8,0,0,0-8,8v24a8,8,0,0,0,16,0V216A8,8,0,0,0,128,208Zm112-88H216a8,8,0,0,0,0,16h24a8,8,0,0,0,0-16Z"></path>
+              </svg>
+            </a>
           </div>
           <div className="flex items-center justify-between gap-3 lg:hidden">
             <button
--- app/layout.tsx
+++ app/layout.tsx
@@ -18,7 +18,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="en">
-      <body suppressHydrationWarning className={inter.className}>
+      <body suppressHydrationWarning className={`${inter.className} dark-theme`}>
         <Navbar />
         <main className="relative pt-20">{children}</main>
         <Footer />
